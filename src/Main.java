58ecdbb22fc578c7eb470e0788f8bc0b61156b10
Anastasia Krapivina <krapivina934@gmail.com>
2023-03-21 23:03:26 +0700

    cheange two

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        int balanceStart = 150; // Начальный счёт
+        int sum = 1200; // Сумма пополнения
+        int bonus;
 
+        if (sum >= 1000) {
+            bonus = sum / 100;
+        } else {
+            bonus = 0;
+        }
 
-        System.out.println("Hello world!");
+        System.out.println(bonus + " - Количество бонусных рублей");
+        System.out.println(balanceStart + sum + bonus + " - Итоговая сумма на счету клиента");
     }
 }
